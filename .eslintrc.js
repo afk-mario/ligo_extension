56f5389d1fb96eefd283e80f1933aae1f5ea25da
Arlefreak <hi@arlefreak.com>
2017-01-12 14:57:34 -0600

    Refactored and added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "extends": "airbnb",
+    "globals":{
+      "document": true,
+      "localStorage": true,
+      "chrome": true,
+      "fetch": true,
+      "Request": true,
+      "Headers": true,
+      "location": true,
+    },
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ]
+};
--- script.js
+++ script.js
@@ -1,93 +1,92 @@
-function reset(){
-    location.reload(true);
+function reset() {
+  location.reload(true);
 }
 
-function resetToken(){
-    console.log('resetToken');
-    localStorage.removeItem('token.afk');
-    reset();
+function resetToken() {
+  localStorage.removeItem('token.afk');
+  reset();
 }
 
-function init(url){
-    console.log('started script');
-    document.getElementById('url').value = url;
-    document.getElementById('reset-token').addEventListener('click', resetToken);
-    var token = localStorage.getItem('token.afk');
-
-    if(token){
-        document.getElementById('submitLinkForm').className = '';
-        document.getElementById('submitTokenForm').className = 'hidden';
-
-        var submitButton = document.getElementById('submitLink');
-        submitButton.addEventListener('click', ()=>{
-            submitLink(token);
-        });
-    }else{
-        document.getElementById('submitLinkForm').className = 'hidden';
-        document.getElementById('submitTokenForm').className = '';
-
-        var submitButton = document.getElementById('submitToken');
-        submitButton.addEventListener('click', ()=>{
-            submitToken();
-        });
-    }
+function submitToken() {
+  const token = document.getElementById('token').value;
+  localStorage.setItem('token.afk', token);
+  document.getElementById('submitLink').value = 'submited';
+  reset();
 }
 
-function submitToken(){
-    var token = document.getElementById('token').value;
-    localStorage.setItem('token.afk', token);
+function parseTags(_tags) {
+  const tags = _tags.split(',');
+  tags.push('fromChrome');
+  return tags;
+}
+
+function submitLink(token) {
+  const tokenHeader = `Token ${token}!`;
+  const headers = new Headers({
+    'Content-Type': 'application/json',
+    Authorization: tokenHeader,
+  });
+  const request = new Request('https://api.arlefreak.com/ligoj/link/', {
+    method: 'POST',
+    redirect: 'follow',
+    mode: 'cors',
+    headers,
+    body: JSON.stringify({
+      link: document.getElementById('url').value,
+      tags: parseTags(document.getElementById('tags').value),
+    }),
+  });
+
+  fetch(request).then((res) => {
     document.getElementById('submitLink').value = 'submited';
-    reset();
-}
-
-function submitLink(token){
-    var tokenHeader = 'Token ' + token;
-    console.log('submitLink token: ' + tokenHeader);
-    var headers = new Headers();
-    var request = new Request('https://api.arlefreak.com/ligoj/link/', {
-        method: 'POST',
-        redirect: 'follow',
-        mode: 'cors',
-        headers: new Headers({
-            'Content-Type': 'application/json',
-            'Authorization': 'Token ' + token  
-        }),
-        body: JSON.stringify({
-            link: document.getElementById('url').value,
-            tags: parseTags(document.getElementById('tags').value)
-        })
-    });
-
-    fetch(request).then((res)=> {
-        console.log(res);
-        document.getElementById('submitLink').value = 'submited';
-        {/* reset(); */}
-    });
-}
-
-function parseTags(_tags){
-    var tags = _tags.split(',');
-    tags.push('fromChrome');
-    return tags;
+  });
 }
 
 function getCurrentTabUrl(callback) {
   // https://developer.chrome.com/extensions/tabs#method-query
-  var queryInfo = {
+  const queryInfo = {
     active: true,
-    currentWindow: true
+    currentWindow: true,
   };
 
-  chrome.tabs.query(queryInfo, function(tabs) {
-    var tab = tabs[0];
-    var url = tab.url;
-    console.assert(typeof url == 'string', 'tab.url should be a string');
+  chrome.tabs.query(queryInfo, (tabs) => {
+    const tab = tabs[0];
+    const url = tab.url;
+    // console.assert(typeof url == 'string', 'tab.url should be a string');
     callback(url);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    getCurrentTabUrl(function(url) {
-        init(url);
+function init(url) {
+  const token = localStorage.getItem('token.afk');
+  const submitButtonLink = document.getElementById('submitLink');
+  const submitButtonToken = document.getElementById('submitToken');
+  const urlField = document.getElementById('url');
+  const resetTokenButton = document.getElementById('reset-token');
+  const submitLinkForm = document.getElementById('submitLinkForm');
+  const submitTokenForm = document.getElementById('submitTokenForm');
+
+  urlField.value = url;
+  resetTokenButton.addEventListener('click', resetToken);
+
+  if (token) {
+    submitLinkForm.className = '';
+    submitTokenForm.className = 'hidden';
+    submitButtonLink.addEventListener('click', () => {
+      submitLink(token);
     });
+  } else {
+    submitLinkForm.className = 'hidden';
+    submitTokenForm.className = '';
+
+    submitButtonToken.addEventListener('click', () => {
+      submitToken();
+    });
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  getCurrentTabUrl((url) => {
+    init(url);
+  });
 });
